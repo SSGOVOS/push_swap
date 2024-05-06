a07188bb864d4d7bd05feaa1b37156d4dfd72f78
amoubine <amoubine@c4r9p2.1337.ma>
2024-05-06 16:56:46 +0100

    govos

--- check2.c
+++ check2.c
@@ -6,7 +6,7 @@
 /*   By: amoubine <amoubine@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 09:36:21 by amoubine          #+#    #+#             */
-/*   Updated: 2024/04/30 10:09:18 by amoubine         ###   ########.fr       */
+/*   Updated: 2024/05/02 19:09:29 by amoubine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_isalpha(char *c)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	sign_check(c);
@@ -26,14 +26,14 @@ void	ft_isalpha(char *c)
 	}
 }
 
-void sign_check(char *s)
+void	sign_check(char *s)
 {
-	char *str;
+	char	*str;
 
 	str = s;
-	while(*str)
+	while (*str)
 	{
-		if(*str == '-' || *str == '+')
+		if (*str == '-' || *str == '+')
 		{
 			str++;
 			if ((*str) == '-' || (*str) == '+')
@@ -43,4 +43,44 @@ void sign_check(char *s)
 		}
 		str++;
 	}
+}
+
+void	ft_strcpy(char *dst, const char *src)
+{
+	size_t i;
+
+	if (dst == NULL)
+		return ;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		res = res * 10 + (str[i++] - 48);
+	}
+	return (res * sign);
 }
--- push_swap.c
+++ push_swap.c
@@ -6,15 +6,83 @@
 /*   By: amoubine <amoubine@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 09:13:19 by amoubine          #+#    #+#             */
-/*   Updated: 2024/04/30 09:20:32 by amoubine         ###   ########.fr       */
+/*   Updated: 2024/05/06 16:49:59 by amoubine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <string.h>
 
-int main(int ac , char **av)
+int	lenght(int ac, char **av)
 {
+	int	i;
+	int	len;
+
+	len = 0;
+	i = 1;
+	while (i < ac)
+	{
+		len += ft_strlen(av[i]) + 1;
+		i++;
+	}
+	return (len);
+}
+char	*convert(int ac, char **av)
+{
+	int		len;
+	char	*result;
+	int		i;
+	int		index;
+
+	len = lenght(ac, av);
+	result = malloc(len);
+	if (!result)
+		return (NULL);
+	i = 1;
+	index = 0;
+	while (i < ac)
+	{
+		strcpy(result + index, av[i]);
+		if (i + 1 < ac)
+        {
+          index = ft_strlen(result);
+          result[index] = ' ';
+        }
+		index++;
+		i++;
+	}
+	result[len - 1] = '\0';
+	return (result);
+}
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	char *str;
+	char **ptr;
+	int i;
+	int number;
+
+	i = 0;
 	if (ac == 1)
 		return (0);
-	check_ifnumber(ac , av);
+	check_ifnumber(ac, av);
+	str = convert(ac, av);
+	printf("%s\n", str);
+	ptr = ft_split(str , ' ');
+	while (ptr[i])
+	{
+		printf("%s\n", ptr[i]);
+		i++;
+	}
+	i = 0;
+	number = ft_atoi(ptr[i]);
+	while (ptr[i])
+	{
+		printf("%i" , ft_atoi(ptr[i]));
+		i++;
+	}
+
+	free(str);
+	free(ptr);
 }
